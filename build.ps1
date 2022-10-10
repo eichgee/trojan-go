71ddcf7bf369daa964e49f59ea867475d9baa767
eichgee <henrygustian09@gmail.com>
2022-10-10 11:07:28 +0700

    update

--- build.ps1
+++ build.ps1
@@ -4,12 +4,17 @@ param(
 $jni_libs_path = "../src/main/jniLibs/"
 $NDK_ROOT = $Env:ANDROID_NDK_HOME+"\23.1.7779620\toolchains\llvm\prebuilt\windows-x86_64\bin"
 
+$PACKAGE_NAME = "github.com/p4gefau1t/trojan-go"
+$VERSION = git describe --tag --abbrev=0
+$COMMIT = git rev-parse HEAD
+$VAR_SETTING = "-X $PACKAGE_NAME/constant.Version=$VERSION -X $PACKAGE_NAME/constant.Commit=$COMMIT"
+
 function linux_386{
     $path = $jni_libs_path+"x86"
     New-Item -ItemType Directory -Force -Path $path
     go env -u GOARM
     go env -w CGO_ENABLED=1 GOARCH=386 GOOS=android CC=$NDK_ROOT\i686-linux-android21-clang
-    go build -v -tags "full" -trimpath -ldflags="-s -w" -o $path/libtrojango.so
+    go build -v -tags "full" -trimpath -ldflags="-s -w -buildid= $VAR_SETTING" -o $path/trojango.exe
 }
 
 function linux_amd64{
@@ -17,13 +22,13 @@ function linux_amd64{
     New-Item -ItemType Directory -Force -Path $path
     go env -u GOARM
     go env -w CGO_ENABLED=1 GOARCH=amd64 GOOS=android CC=$NDK_ROOT\x86_64-linux-android21-clang
-    go build -v -tags "full" -trimpath -ldflags="-s -w" -o $path/libtrojango.so
+    go build -v -tags "full" -trimpath -ldflags="-s -w -buildid= $VAR_SETTING" -o $path/trojango.exe
 }
 function linux_armv7{
     $path = $jni_libs_path+"armeabi-v7a"
     New-Item -ItemType Directory -Force -Path $path
     go env -w CGO_ENABLED=1 GOARCH=arm GOOS=android GOARM=7 CC=$NDK_ROOT\armv7a-linux-androideabi21-clang
-    go build -v -tags "full" -trimpath -ldflags="-s -w" -o $path/libtrojango.so
+    go build -v -tags "full" -trimpath -ldflags="-s -w -buildid= $VAR_SETTING" -o $path/trojango.exe
 }
 
 function linux_armv8{
@@ -31,7 +36,7 @@ function linux_armv8{
     New-Item -ItemType Directory -Force -Path $path
     go env -u GOARM
     go env -w CGO_ENABLED=1 GOARCH=arm64 GOOS=android CC=$NDK_ROOT\aarch64-linux-android21-clang
-    go build -v -tags "full" -trimpath -ldflags="-s -w" -o $path/libtrojango.so
+    go build -v -tags "full" -trimpath -ldflags="-s -w -buildid= $VAR_SETTING" -o $path/trojango.exe
 }
 
 function win_386{
@@ -39,7 +44,7 @@ function win_386{
     New-Item -ItemType Directory -Force -Path $path
     go env -u GOARM
     go env -w CGO_ENABLED=0 GOARCH=386 GOOS=windows
-    go build -v -tags "full" -trimpath -ldflags="-s -w" -o $path/trojango.exe
+    go build -v -tags "full" -trimpath -ldflags="-s -w -buildid= $VAR_SETTING" -o $path/trojango.exe
 }
 
 function win_amd64{
@@ -47,7 +52,7 @@ function win_amd64{
     New-Item -ItemType Directory -Force -Path $path
     go env -u GOARM
     go env -w CGO_ENABLED=0 GOARCH=amd64 GOOS=windows
-    go build -v -tags "full" -trimpath -ldflags="-s -w" -o $path/trojango.exe
+    go build -v -tags "full" -trimpath -ldflags="-s -w -buildid= $VAR_SETTING" -o $path/trojango.exe
 }
 
 if ($method_name -eq "linux_386"){
